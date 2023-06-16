d4f6545a9f0cb42fde61939c925ce755d85d1526
harry park <harrytucker1016@gmail.com>
2023-06-16 20:03:44 +0900

    Repository 분리 오류해결 테이블에 password not null 해제

---
+++ src/main/java/com/sparta/spartaboard/repository/BoardRepository.java
@@ -0,0 +1,86 @@
+package com.sparta.spartaboard.repository;
+
+import com.sparta.spartaboard.dto.BoardRequestDto;
+import com.sparta.spartaboard.dto.BoardResponseDto;
+import com.sparta.spartaboard.entity.Board;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+//Repository DB관리, DB CRUD 작업처리
+public class BoardRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+    public BoardRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Board save(Board board) {
+        //DB저장 
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
+
+        String sql = "INSERT INTO board (username, contents) VALUES (?, ?)";
+        //이름과 내용은 항상 바뀌기 때문에 동적으로 처리하기위해 ? 로 넣음
+        jdbcTemplate.update(con -> {
+                    PreparedStatement preparedStatement = con.prepareStatement(sql,
+                            Statement.RETURN_GENERATED_KEYS);
+
+                    preparedStatement.setString(1, board.getUsername());
+                    preparedStatement.setString(2, board.getContents());
+                    return preparedStatement;
+                },
+                keyHolder);
+
+        // DB Insert 후 받아온 기본키 확인
+        Long id = keyHolder.getKey().longValue();
+        board.setId(id);
+        
+        return board;
+    }
+    public List<BoardResponseDto> findAll() {
+        String sql = "SELECT * FROM board";
+        return jdbcTemplate.query(sql, new RowMapper<BoardResponseDto>() {
+            @Override
+            public BoardResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+                // SQL 의 결과로 받아온 Memo 데이터들을 MemoResponseDto 타입으로 변환해줄 메서드
+                Long id = rs.getLong("id");
+                String username = rs.getString("username");
+                String contents = rs.getString("contents");
+                return new BoardResponseDto(id, username, contents);
+            }
+        });
+    }
+    public Board findById(Long id) {
+        // DB 조회
+        String sql = "SELECT * FROM board WHERE id = ?";
+
+        return jdbcTemplate.query(sql, resultSet -> {
+            if (resultSet.next()) {
+                Board board = new Board();
+                board.setUsername(resultSet.getString("username"));
+                board.setContents(resultSet.getString("contents"));
+                return board;
+            } else {
+                return null;
+            }
+        }, id);
+
+    }
+
+    public void update(Long id, BoardRequestDto requestDto) {
+        String sql = "UPDATE board SET username = ?, contents = ? WHERE id = ?";
+        jdbcTemplate.update(sql, requestDto.getUsername(), requestDto.getContents(), id);
+    }
+
+    public void delete(Long id) {
+        String sql = "DELETE FROM board WHERE id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+}
--- src/main/java/com/sparta/spartaboard/service/BoardService.java
+++ src/main/java/com/sparta/spartaboard/service/BoardService.java
@@ -3,6 +3,7 @@ package com.sparta.spartaboard.service;
 import com.sparta.spartaboard.dto.BoardRequestDto;
 import com.sparta.spartaboard.dto.BoardResponseDto;
 import com.sparta.spartaboard.entity.Board;
+import com.sparta.spartaboard.repository.BoardRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -27,25 +28,11 @@ public class BoardService {
     public BoardResponseDto createPost(BoardRequestDto requestDto) {
         //RequestDto -> Entity (데이터베이스에 저장)
         Board board = new Board(requestDto); // requestDto로 클라이언트에서 받아온 데이터
+        //board 클래스 객체 하나가 데이터베이스의 한줄, 한row다
 
         // DB 저장
-        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본 키를 반환받기 위한 객체
-
-        String sql = "INSERT INTO board (username, contents) VALUES (?, ?)";
-        //이름과 내용은 항상 바뀌기 때문에 동적으로 처리하기위해 ? 로 넣음
-        jdbcTemplate.update(con -> {
-                    PreparedStatement preparedStatement = con.prepareStatement(sql,
-                            Statement.RETURN_GENERATED_KEYS);
-
-                    preparedStatement.setString(1, board.getUsername());
-                    preparedStatement.setString(2, board.getContents());
-                    return preparedStatement;
-                },
-                keyHolder);
-
-        // DB Insert 후 받아온 기본키 확인
-        Long id = keyHolder.getKey().longValue();
-        board.setId(id);
+        BoardRepository boardRepository = new BoardRepository(jdbcTemplate);
+        Board savePost = boardRepository.save(board);
 
         //Entity - > ResponseDto
         BoardResponseDto boardResponseDto = new BoardResponseDto(board);
@@ -55,25 +42,18 @@ public class BoardService {
 
     public List<BoardResponseDto> getPost() {
         //DB조회
-        String sql = "SELECT * FROM board";
-        return jdbcTemplate.query(sql, new RowMapper<BoardResponseDto>() {
-            @Override
-            public BoardResponseDto mapRow(ResultSet rs, int rowNum) throws SQLException {
-                // SQL 의 결과로 받아온 Memo 데이터들을 MemoResponseDto 타입으로 변환해줄 메서드
-                Long id = rs.getLong("id");
-                String username = rs.getString("username");
-                String contents = rs.getString("contents");
-                return new BoardResponseDto(id, username, contents);
-            }
-        });
+        BoardRepository boardRepository = new BoardRepository(jdbcTemplate);
+        return boardRepository.findAll();
+
     }
 
     public Long updatePost(Long id, BoardRequestDto requestDto) {
-        Board board = findById(id);
+        BoardRepository boardRepository = new BoardRepository(jdbcTemplate);
+
+        Board board = boardRepository.findById(id);
         if (board != null) {
             //게시글 내용 수정
-            String sql = "UPDATE board SET username = ?, contents = ? WHERE id = ?";
-            jdbcTemplate.update(sql, requestDto.getUsername(), requestDto.getContents(), id);
+            boardRepository.update(id,requestDto);
 
             return id;
         } else {
@@ -82,41 +62,26 @@ public class BoardService {
     }
 
     public Long selectedPost(Long id) {
-        Board board = findById(id);
+        BoardRepository boardRepository = new BoardRepository(jdbcTemplate);
+        Board board = boardRepository.findById(id);
         if (board != null) {
             return id;
         } else {
             throw new IllegalArgumentException("선택한 게시글은 존재하지 않습니다");
         }
     }
-
 
     public Long deletePost(Long id) {
-        Board board = findById(id);
+        BoardRepository boardRepository = new BoardRepository(jdbcTemplate);
+
+        Board board = boardRepository.findById(id);
         if (board != null) {
             //게시글 삭제
-            String sql = "DELETE FROM board WHERE id = ?";
-            jdbcTemplate.update(sql, id);
-
+            boardRepository.delete(id);
             return id;
         } else {
             throw new IllegalArgumentException("선택한 게시글은 존재하지 않습니다");
         }
     }
-    private Board findById(Long id) {
-        // DB 조회
-        String sql = "SELECT * FROM board WHERE id = ?";
 
-        return jdbcTemplate.query(sql, resultSet -> {
-            if (resultSet.next()) {
-                Board board = new Board();
-                board.setUsername(resultSet.getString("username"));
-                board.setContents(resultSet.getString("contents"));
-                return board;
-            } else {
-                return null;
-            }
-        }, id);
-
-    }
 }
